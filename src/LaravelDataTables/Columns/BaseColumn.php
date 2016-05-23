c637ecbbe977938208205065de5b5f90ad1d62dc
Daniel Withams <dan@7d-digital.co.uk>
2016-05-23 11:45:10 +0100

    Auto-setting the Title to something more human readable

--- src/LaravelDataTables/Columns/BaseColumn.php
+++ src/LaravelDataTables/Columns/BaseColumn.php
@@ -53,7 +53,8 @@ abstract class BaseColumn
         $this->setColumnAttribute('columnName', $value);
 
         if (is_null($this->getName())) {
-            $this->setName($value);
+			$name = Stringy::create($value)->humanize();
+            $this->setName($name);
         }
 
         return $this;

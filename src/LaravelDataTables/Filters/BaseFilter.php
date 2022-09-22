a8915c53b71e3992a9c1f155067b60793211e5fa
Daniel Withams <dan@7d-digital.co.uk>
2022-09-22 12:53:17 +0100

    Fixing error

--- src/LaravelDataTables/Filters/BaseFilter.php
+++ src/LaravelDataTables/Filters/BaseFilter.php
@@ -12,7 +12,7 @@ abstract class BaseFilter
     public function __construct($relationshipPath, $requestPath)
     {
         $this->setRelationshipPath($relationshipPath);
-        $this->setRequestPath($relationshipPath);
+        $this->setRequestPath($requestPath);
         return $this;
     }
 

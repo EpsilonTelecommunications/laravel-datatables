8647c6bef2b9d29b34d92b81d2253a46452d8a30
Leonardo-Atalla <l.atalla@epsilontel.com>
2023-06-29 16:04:28 +0300

    add get set template with fallback on parent build template. override class buildHtml to return current template.

--- src/LaravelDataTables/Filters/SelectMultipleFilter.php
+++ src/LaravelDataTables/Filters/SelectMultipleFilter.php
@@ -5,12 +5,29 @@ namespace SevenD\LaravelDataTables\Filters;
 class SelectMultipleFilter extends SelectFilter
 {
     protected $multiple = true;
+    protected $template;
 
     public function getSettings(): array
     {
         return array_merge(parent::getSettings(), [
             'multiple' => $this->isMultiple()
         ]);
+    }
+
+    public function buildHtml(): string
+    {
+        return $this->getTemplate();
+    }
+
+    public function setTemplate($htmlTemplate): static
+    {
+        $this->template = $htmlTemplate;
+        return $this;
+    }
+
+    public function getTemplate(): string
+    {
+        return $this->template ?? parent::buildHtml();
     }
 
     protected function toJSObject($data)
